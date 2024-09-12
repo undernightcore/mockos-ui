4410dd6d73b53debf2f4eac4e4266d04be4aadf7
undernightcore <undernightcore@gmail.com>
2024-09-12 22:15:51 +0200

    feat: create home header animation

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,19 @@
 import { Component } from '@angular/core';
 import { AppManagerService } from '../../services/app/app-manager.service';
+import {
+  debounce,
+  exhaustMap,
+  fromEvent,
+  map,
+  of,
+  shareReplay,
+  switchMap,
+  take,
+  tap,
+  throttle,
+  throttleTime,
+  timer,
+} from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +21,15 @@ import { AppManagerService } from '../../services/app/app-manager.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  sticky$ = fromEvent<{ target: HTMLDivElement }>(window, 'scroll', {
+    capture: true,
+  }).pipe(
+    map(() => true),
+    take(1),
+    shareReplay(1)
+  );
+
   constructor(appManager: AppManagerService) {
-    appManager.setHeaderData({});
+    appManager.setHeaderData({ hideHeader: true });
   }
 }
