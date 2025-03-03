47ab720114fbb21b8d346ee78e8d3618f655e918
JuanCGit <juancasanovagago@gmail.com>
2025-03-03 23:30:07 +0100

    feat: almost finish modal logic

--- src/app/components/headers-modal/headers-modal.module.ts
+++ src/app/components/headers-modal/headers-modal.module.ts
@@ -6,6 +6,7 @@ import { DialogModule } from '../dialog/dialog.module';
 import { ButtonModule } from '../button/button.module';
 import {InputModule} from "../input/input.module";
 import {HeaderModule} from "./components/header.module";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [HeadersModalComponent],
@@ -16,6 +17,7 @@ import {HeaderModule} from "./components/header.module";
     ButtonModule,
     InputModule,
     HeaderModule,
+    ReactiveFormsModule,
   ],
   exports: [HeadersModalComponent],
 })
